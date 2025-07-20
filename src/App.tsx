5a393b2bcd49aae903f85e2f4a18e5f8332143d4
bemnetgetasetegn <bigbemnet@gmail.com>
2025-07-20 20:08:51 +0300

    fixed app.tsx error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Features from "./Components/Features";
 import Hero from "./Components/Hero";
 import HoneyFooter from "./Components/HoneyFooter";
 import HoneyServiceSection from "./Components/HoneyServiceSection";
-import NavBar from "./Components/navbar";
+import NavBar from "./Components/NavBar";
 import Testimonial from "./Components/Testimonials";
 
 const App = () => {
